28bd00a5283540ef7ab3d4e770fe7d2f3e066849
Ben Limpanukorn <benmomo1@gmail.com>
2014-10-31 16:58:49 -0700

    Added HolonomicDriveTest

---
+++ 2014-2015/JoystickControllerTest.c
@@ -0,0 +1,25 @@
+
+#include "JoystickDriver.c"
+
+// Constants
+#define WAIT_TIME		5 // Milliseconds
+
+task main() {
+	while (true) {
+		//writeDebugStreamLine("Loop");
+		getJoystickSettings(joystick); // Update joystick
+		if (joy1Btn(4)) {
+			writeDebugStreamLine("Button 4 Pressed");
+		}
+		if (joy1Btn(2)) {
+			writeDebugStreamLine("Button 2 Pressed");
+		}
+		//writeDebugStreamLine("%d", joystick.joy1_y1);
+
+		//writeDebugStreamLine("%d", joy1Btn(4));
+
+		//writeDebugStreamLine("%d", joystick.joy1_Buttons);
+
+		wait1Msec(WAIT_TIME);
+	}
+}
