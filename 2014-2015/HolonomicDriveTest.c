189eb69af081087acfae371e3df1df7e013ad61f
Ben Limpanukorn <benmomo1@gmail.com>
2014-11-09 18:15:04 -0800

    Finished Holonomic Drive
    Not tested with actual robot yet.

--- 2014-2015/HolonomicDriveTest.c
+++ 2014-2015/HolonomicDriveTest.c
@@ -19,30 +19,27 @@
 
 #define FULL_MOTOR_POWER 90
 
-task main() {
-	while (true) {
-		getJoystickSettings(joystick);
+#define LEFT_ORIENTATION_HOLO_WHEEL		1
+#define RIGHT_ORIENTATION_HOLO_WHEEL	2
 
-	}
-}
+#define LOOP_INTERVAL 5
 
-// Given a point within the Cartesian coordinate system, this function will calculate the motor power based upon the distance from the origin to the given point.
-int getPower(int x, int y) {
-	// x and y must be within [-100, 100]
-	int power;
-	power = sqrt(x^2 + y^2);
-	if (power > 100) {
-		power = 100;
-	} else if (power < -100) {
-		pwoer = -100;
+#define DEADBAND(x) ((abs(x) >= 10)? x: 0)
+
+float modAngle(float angle) {
+	while (angle >= 360) {
+		angle = angle - 360;
 	}
-	return power;
+	while (angle < 0) {
+		angle = angle + 360;
+	}
+	return angle;
 }
 
 // Given a point within the Cartesian coordinate system, this function will calculate the angle, in standard position, from the origin to the given point.
-int getAngle(int x, int y) {
-	int angle		= 0;
-	int refAngle	= 0;
+float getAngle(float x, float y) {
+	float angle	= 0;
+	float refAngle	= 0;
 	//int quadrant	= 0;
 
 	/*
@@ -65,7 +62,7 @@ int getAngle(int x, int y) {
 	// Find angle in Standard Position
 	if (x != 0 && y != 0) { // Exclude any Quadrantal Angles
 		// Find Reference Angle
-		refAngle = atan(y/x);
+		refAngle = atan(y/x)*180/PI;
 
 		// Find Angle in Standard Position
 		if (x < 0) { // Quadrant II or III
@@ -75,12 +72,7 @@ int getAngle(int x, int y) {
 		}
 
 		// Ensure angle is within bounds: [0, 360)
-		while (angle >= 360) {
-			angle = angle - 360;
-		}
-		while (angle < 0) {
-			angle = angle + 360;
-		}
+		angle = modAngle(angle);
 	}
 	// Old Algorithm
 	/*
@@ -127,6 +119,64 @@ int getAngle(int x, int y) {
   //	writeDebugStreamLine("ERROR: Invalid value for 'x'. Value must be an integer within the bounds: [-100,100].");
 	//}
 	return angle;
+}
+
+float getRefAngle(float angle) {
+	float refAngle;
+	if			(angle < 90  && angle > 0) {
+		refAngle = angle;
+	} else if	(angle < 180 && angle > 90) {
+		refAngle = 180 - angle;
+	} else if	(angle < 270 && angle > 180) {
+		refAngle = angle - 180;
+	} else if	(angle < 360 && angle > 270) {
+		refAngle = 360 - angle;
+	}
+
+	// Quadrantal Angles
+	if (angle == 0 || angle == 180) {
+		refAngle = 0;
+	} else if (angle == 90 || angle == 270) {
+		refAngle = 90;
+	}
+	return refAngle;
+}
+
+float remapJoystickInput(float joy) { // Remaps Joystick input [-128, 127] to [-100, 100]
+	return (joy/128)*100;
+}
+
+// Given a point within the Cartesian coordinate system, this function will calculate the motor power based upon the distance from the origin to the given point.
+float getPower(float x, float y) {
+	// x and y must be within [-100, 100]
+	float power;
+	power = sqrt((x*x) + (y*y));
+	if (power > 100) {
+		power = 100;
+	} else if (power < -100) {
+		power = -100;
+	}
+	return power/100;
+}
+
+float getMotorPower(float power, float direction, int wheelOri) {
+	float finPower;
+	float tempDir;
+
+	if (wheelOri == LEFT_ORIENTATION_HOLO_WHEEL) {
+		tempDir = modAngle(direction - 45);
+	} else if (wheelOri == RIGHT_ORIENTATION_HOLO_WHEEL) {
+		tempDir = modAngle(direction + 45);
+	}
+
+	finPower = getRefAngle(tempDir)/90*100;
+	if (tempDir > 180) {
+		finPower = -finPower;
+	}
+
+	finPower = finPower*power;
+
+	return finPower;
 }
 
 void holoDrive(int joy1_x, int joy1_y, int joy2_x, int joy2_y) {
@@ -177,4 +227,29 @@ void simpleHoloMove(int movement) {
 void simpleHoloRotate(int rotation) {
 	// Rotation input must be: {1, 2} corresponding to {rotateLeft, rotateRight}
 
+}
+
+task main() {
+	while (true) {
+		wait1Msec(LOOP_INTERVAL);
+		getJoystickSettings(joystick);
+
+		//writeDebugStreamLine("Looop");
+		//writeDebugStreamLine("%d", joy1_x1);
+		/*
+		if (joystick.joy1_x1 > 50) {
+			writeDebugStreamLine("JOY");
+		}
+		*/
+		if (joy1Btn(4)) {
+			writeDebugStreamLine("JOY4BUTTON");
+		}
+
+		// TEST CODE
+		writeDebugStreamLine("%f", getMotorPower(getPower(remapJoystickInput(DEADBAND(joystick.joy1_x1)), remapJoystickInput(DEADBAND(joystick.joy1_y1))), getAngle(remapJoystickInput(DEADBAND(joystick.joy1_x1)), remapJoystickInput(DEADBAND(joystick.joy1_y1))), RIGHT_ORIENTATION_HOLO_WHEEL));
+		//writeDebugStreamLine("%d", joystick.joy1_y1);
+		//writeDebugStreamLine("%f", getRefAngle(getAngle(remapJoystickInput(DEADBAND(joystick.joy1_x1)), remapJoystickInput(DEADBAND(joystick.joy1_y1))));
+		//writeDebugStreamLine("%f", getPower(remapJoystickInput(DEADBAND(joystick.joy1_x1)), remapJoystickInput(DEADBAND(joystick.joy1_y1))));
+		//writeDebugStreamLine("%f", getAngle(remapJoystickInput(joystick.joy1_x1), remapJoystickInput(joystick.joy1_y1)));
+	}
 }
