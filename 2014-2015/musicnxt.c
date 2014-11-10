8c8d5f6b5a8a5c81e4779771d6c1d06040b8b2bc
Ben Limpanukorn <benmomo1@gmail.com>
2014-11-09 18:16:22 -0800

    Added music for the NXT

---
+++ 2014-2015/musicnxt.c
@@ -0,0 +1,120 @@
+
+
+void MissionImpossible()
+{
+  //        100 = Tempo
+  //          6 = Default octave
+  //    Quarter = Default note length
+  //        10% = Break between notes
+  //
+  PlayTone(  880,    7); wait1Msec(  75);  // Note(D, Duration(32th))
+  PlayTone(  933,    7); wait1Msec(  75);  // Note(D#, Duration(32th))
+  PlayTone(  880,    7); wait1Msec(  75);  // Note(D, Duration(32th))
+  PlayTone(  933,    7); wait1Msec(  75);  // Note(D#, Duration(32th))
+  PlayTone(  880,    7); wait1Msec(  75);  // Note(D, Duration(32th))
+  PlayTone(  933,    7); wait1Msec(  75);  // Note(D#, Duration(32th))
+  PlayTone(  880,    7); wait1Msec(  75);  // Note(D, Duration(32th))
+  PlayTone(  933,    7); wait1Msec(  75);  // Note(D#, Duration(32th))
+  PlayTone(  880,    7); wait1Msec(  75);  // Note(D, Duration(32th))
+  PlayTone(  880,    7); wait1Msec(  75);  // Note(D, Duration(32th))
+  PlayTone(  933,    7); wait1Msec(  75);  // Note(D#, Duration(32th))
+  PlayTone(  988,    7); wait1Msec(  75);  // Note(E, Duration(32th))
+  PlayTone( 1047,    7); wait1Msec(  75);  // Note(F, Duration(32th))
+  PlayTone( 1109,    7); wait1Msec(  75);  // Note(F#, Duration(32th))
+  PlayTone( 1175,    7); wait1Msec(  75);  // Note(G, Duration(32th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone(  784,   14); wait1Msec( 150);  // Note(C, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1047,   14); wait1Msec( 150);  // Note(F, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1109,   14); wait1Msec( 150);  // Note(F#, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone(  784,   14); wait1Msec( 150);  // Note(C, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1047,   14); wait1Msec( 150);  // Note(F, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1109,   14); wait1Msec( 150);  // Note(F#, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(  880,  108); wait1Msec(1200);  // Note(D, Duration(Half))
+  PlayTone(    0,    7); wait1Msec(  75);  // Note(Rest, Duration(32th))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(  831,  108); wait1Msec(1200);  // Note(C#, Duration(Half))
+  PlayTone(    0,    7); wait1Msec(  75);  // Note(Rest, Duration(32th))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(  784,  108); wait1Msec(1200);  // Note(C, Duration(Half))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone(  932,   14); wait1Msec( 150);  // Note(A#5, Duration(16th))
+  PlayTone(  784,   14); wait1Msec( 150);  // Note(C, Duration(16th))
+  return;
+}
+void Vivaldi()
+{
+  //        160 = Tempo
+  //          6 = Default octave
+  //    Quarter = Default note length
+  //        10% = Break between notes
+  //
+  PlayTone(  880,   17); wait1Msec( 188);  // Note(A5, Duration(Eighth))
+  PlayTone(  988,   17); wait1Msec( 188);  // Note(E, Duration(Eighth))
+  PlayTone( 1046,   17); wait1Msec( 188);  // Note(C7, Duration(Eighth))
+  PlayTone(  988,   17); wait1Msec( 188);  // Note(E, Duration(Eighth))
+  PlayTone( 1174,   17); wait1Msec( 188);  // Note(D7, Duration(Eighth))
+  PlayTone(  880,   17); wait1Msec( 188);  // Note(A5, Duration(Eighth))
+  PlayTone( 1046,   17); wait1Msec( 188);  // Note(C7, Duration(Eighth))
+  PlayTone(  880,   34); wait1Msec( 375);  // Note(D)
+  PlayTone(  783,   17); wait1Msec( 188);  // Note(G5, Duration(Eighth))
+  PlayTone(  988,   17); wait1Msec( 188);  // Note(E, Duration(Eighth))
+  PlayTone( 1481,   17); wait1Msec( 188);  // Note(B, Duration(Eighth))
+  PlayTone(  988,   17); wait1Msec( 188);  // Note(E, Duration(Eighth))
+  PlayTone( 1046,   17); wait1Msec( 188);  // Note(C7, Duration(Eighth))
+  PlayTone(  988,   17); wait1Msec( 188);  // Note(E, Duration(Eighth))
+  PlayTone( 1481,   17); wait1Msec( 188);  // Note(B, Duration(Eighth))
+  PlayTone( 1320,   34); wait1Msec( 375);  // Note(A)
+  PlayTone(  698,   17); wait1Msec( 188);  // Note(F5, Duration(Eighth))
+  PlayTone(  880,   17); wait1Msec( 188);  // Note(D, Duration(Eighth))
+  PlayTone( 1320,   17); wait1Msec( 188);  // Note(A, Duration(Eighth))
+  PlayTone(  880,   17); wait1Msec( 188);  // Note(D, Duration(Eighth))
+  PlayTone( 1481,   17); wait1Msec( 188);  // Note(B, Duration(Eighth))
+  PlayTone(  698,   17); wait1Msec( 188);  // Note(F5, Duration(Eighth))
+  PlayTone( 1320,   17); wait1Msec( 188);  // Note(A, Duration(Eighth))
+  PlayTone( 1109,   17); wait1Msec( 188);  // Note(F#, Duration(Eighth))
+  PlayTone(  659,   17); wait1Msec( 188);  // Note(E5, Duration(Eighth))
+  PlayTone( 1109,   17); wait1Msec( 188);  // Note(F#, Duration(Eighth))
+  PlayTone(  659,   17); wait1Msec( 188);  // Note(E5, Duration(Eighth))
+  PlayTone( 1320,   17); wait1Msec( 188);  // Note(A, Duration(Eighth))
+  PlayTone(  659,   17); wait1Msec( 188);  // Note(E5, Duration(Eighth))
+  PlayTone( 1481,   17); wait1Msec( 188);  // Note(B, Duration(Eighth))
+  PlayTone(  659,   34); wait1Msec( 375);  // Note(E5)
+  return;
+}
+
+
+
+task main()
+{
+	Vivaldi();
+	MissionImpossible();
+}
